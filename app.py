9562e29e339eef9b38a21102426116b626f98536
Matti <25400788+MathiasDevelopes@users.noreply.github.com>
2026-02-04 13:03:37 +0100

    feat: fasitsvar

--- app.py
+++ app.py
@@ -2,9 +2,38 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World'
+@app.route('/joke')
+def joke():
+    return "Why did the chicken cross the road? No idea."
+
+@app.route('/hello/<string:name>')
+def hello(name: str):
+    return "Hei " + name
+
+# Merk deg at vi bruker <int:num> for tall her, da sikrer vi at det vi får som input er en integer (tall).
+@app.route('/calculate/<int:num1>/<string:operator>/<int:num2>')
+def calculate(num1: int, operator: str, num2: int):
+
+    # Her bruker vi en "match" statement for å sjekke om operator er en av de gyldige du kan bruke.
+    match operator:
+        case 'pluss':
+            # Pluss sammen tallene
+            return str(num1+num2)
+        case 'minus':
+            # Subtraher tallene.
+            return str(num1-num2)
+        case 'ganger':
+            # Gang sammen tallene
+            return str(num1*num2)
+        case 'del':
+            # Del a på b.
+            # OBS: Her er det viktig at vi sjekker om num2 er 0, siden vi kan ikke dele ting på 0.
+            if num2 == 0:
+                return "Det er ikke mulig å dele " num1 + " på " + num2
+            return str(num1/num2)
+        # Hvis brukeren når denne casen, betyr det at brukeren har spesifisert en ugyldig operatør.
+        case _:
+            return "Ugyldig operatør " + operator
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
